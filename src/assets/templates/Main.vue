be184cac26deb86fbf86f8dd57a2d23fa1b4ee3d
ramazan <r.abdullin@codezavod.ru>
2020-03-19 17:11:18 +0300

    Add top banners

--- src/assets/templates/Main.vue
+++ src/assets/templates/Main.vue
@@ -1,13 +1,45 @@
 <template>
-    <h1>Hello World</h1>
+    <main class="container">
+        <img src="@/assets/img/main-banner.png" class="img-contain img-contain--width" alt="main-banner">
+        
+        <div class="content">
+            <article class="content__main">
+                <div class="banner-top">
+                    <div class="banner-top__left">
+                        <img src="@/assets/img/uniteller.png" class="img-contain img-contain--width" alt="uniteller">
+                    </div>
+            
+                    <div class="spacer">
+                    </div>
+            
+                    <div class="banner-top__right">
+                        <icon :name="'mir'"></icon>
+                    </div>
+                    <div class="banner-top__right">
+                        <icon :name="'visa'"></icon>
+                    </div>
+                    <div class="banner-top__right">
+                        <svg v-svg symbol="maestro" class="svg"></svg>
+                    </div>
+                    <div class="banner-top__right">
+                        <svg v-svg symbol="mastercard" class="svg"></svg>
+                    </div>
+                </div>
+            </article>
+            <aside class="content__aside"></aside>
+        </div>
+    </main>
 </template>
 
 <script>
-    import {store} from '../js/store/index';
-
+    import {store} from '@/assets/js/store/index';
+    import Icon from '@/assets/templates/Icon';
+    
     export default {
         name: "Main",
-        components: {},
+        components: {
+            Icon
+        },
         store,
         data() {
             return {
