8c5e2751302e87254edb963cd9e79f08d993f09d
ramazan <r.abdullin@codezavod.ru>
2020-03-19 14:05:26 +0300

    Add svg sprite generator

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 //Styles
-import './assets/styles/main.scss';
+import '@/assets/styles/main.scss';
 
 //Libraries
 import Vue from "vue";
+import SvgSprite from 'vue-svg-sprite';
+Vue.use(SvgSprite,{
+  url: '/svg/sprite.svg',
+});
+
 
 //Templates
-import Main from "./assets/templates/Main.vue";
+import Main from "@/assets/templates/Main.vue";
+
 
 //Start Vue
 Vue.config.productionTip = false;
@@ -13,3 +19,11 @@ Vue.config.productionTip = false;
 new Vue({
   render: h => h(Main)
 }).$mount("#app");
+
+
+// Load svg sprite
+function requireAll(r) {
+  r.keys().forEach(r);
+}
+
+requireAll(require.context('@/assets/img/icons/', true, /\.svg$/));
