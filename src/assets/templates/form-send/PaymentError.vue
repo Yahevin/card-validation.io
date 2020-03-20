52f98cd67c2fc52367e8c63f2d7c64718a8fda24
ramazan <r.abdullin@codezavod.ru>
2020-03-20 18:21:15 +0300

    Add 'payment send' templates

---
+++ src/assets/templates/form-send/PaymentError.vue
@@ -0,0 +1,36 @@
+<template>
+	<section class="payment payment-send">
+		<div class="payment-send__status">
+			<svg v-svg symbol="error" class="svg"></svg>
+		</div>
+		
+		<h1 class="payment-send__title">
+			Ошибка
+		</h1>
+		
+		<div class="payment-send__text">
+			<p>
+				Пожалуйста, проверьте правильность введенной информации по Вашей банковской карте и попробуйте еще раз.
+			</p>
+			<br/>
+			<p>
+				Просим Вас повторно ввести поля: Номер карты, Срок действия, Код CVC2 или CVV2, Имя держателя карты.
+				Мы не сохраняем их в целях Вашей безопасности.
+			</p>
+		</div>
+		
+		<div class="payment-send__btn btn-submit">
+			<span class="btn-submit__text">Повторить</span>
+		</div>
+	</section>
+</template>
+
+<script>
+	export default {
+		name: "PaymentError"
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/assets/templates/form-send/PaymentSuccess.vue
@@ -0,0 +1,58 @@
+<template>
+	<section class="payment payment-send">
+		<div class="payment-send__status">
+			<svg v-svg symbol="success" class="svg"></svg>
+		</div>
+		
+		<h1 class="payment-send__title">
+			Платёж <br/> успешно совершён
+		</h1>
+		
+		<p class="payment-send__text">
+			Подробная информация о Вашем платеже отправлена на электронный адрес, указанный во время платежа.
+		</p>
+		
+		<div class="payment-send__btn btn-submit">
+			<span class="btn-submit__text">Вернуться в магазин</span>
+		</div>
+		
+		<div class="payment-send__hr"></div>
+		
+		<div class="payment-send__info pay-cheque">
+			<div class="pay-cheque__position">Дата и время платежа</div>
+			<div class="pay-cheque__position">Получатель платежа</div>
+			<div class="pay-cheque__position">Номер платежа</div>
+			<div class="pay-cheque__position">Сумма</div>
+			<div class="pay-cheque__position">Банковская карта</div>
+			<div class="pay-cheque__position">Код авторизации</div>
+			<div class="pay-cheque__data">01.12.2019 11:13:37</div>
+			<div class="pay-cheque__data">МКУ «Организатор городского парковочного пространства» parkingkzn.ru</div>
+			<div class="pay-cheque__data">474426</div>
+			<div class="pay-cheque__data">404.00 RUB</div>
+			<div class="pay-cheque__data">434412******1044</div>
+			<div class="pay-cheque__data">200.00 RUB</div>
+		</div>
+		
+		<div class="payment-send__print print-btn">
+			<div class="print-btn__wrap">
+				<div class="print-btn__icon">
+					<svg v-svg symbol="printer" class="svg"></svg>
+				</div>
+				<span class="print-btn__text">
+                                Распечатать
+                            </span>
+			</div>
+		</div>
+	
+	</section>
+</template>
+
+<script>
+	export default {
+		name: "PaymentSuccess"
+	}
+</script>
+
+<style scoped>
+
+</style>
